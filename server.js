efc774cd28396d6c8f78bf4eaf223da86ced3728
Venera Aitkul <veneraaitkul@gmail.com>
2020-07-25 18:00:28 +0100

    graphql project using SpaceX Api

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express");
-const {graphqlHTTP} = require("express-graphql");
+const { graphqlHTTP } = require("express-graphql");
+const cors = require("cors");
 const schema = require("./schema");
 
 const app = express();
+
+// by running localhost:3000 we're trying to make request to localhost:5000, our server is not letting us because of security issues
+// Allow cross-origin
+app.use(cors());
 
 // in graphql-express we dont create different routes
 // we'll create a schema file and put there all queries and mutations
